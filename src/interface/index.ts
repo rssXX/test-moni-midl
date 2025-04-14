04f1cc508e9fb706afa3f55670beb3e68767e243
Romanov <rss00@yandex.ru>
2025-04-15 00:01:02 +0500

    типы

---
+++ src/interface/index.ts
@@ -0,0 +1,63 @@
+export type Currency = 'USD';
+
+export type CurrencyValueMap = Record<Currency, string>;
+
+export type Chain = {
+    id: number;
+    name: string;
+    slug: string;
+    logoUrl: string;
+};
+
+export type Platform = {
+    id: number;
+    name: string;
+    slug: string;
+    logoUrl: string;
+};
+
+export type SecurityFeature = {
+    shortName: string;
+    name: string;
+    status: boolean;
+};
+
+export type Link = {
+    name: string;
+    linkUrl: string;
+    logoUrl: string;
+};
+
+export interface Item {
+    id: number;
+    address: string;
+    logoUrl: string;
+    name: string;
+    symbol: string;
+    chain: Chain;
+    platform: Platform;
+    createdAt: number;
+    smartFollowersCount: number;
+    smartFollowersCountChange: number;
+    smartMentionsCount: number;
+    smartMentionsCountChange: number;
+    txsBuyCount: number;
+    txsSellCount: number;
+    txsCountChange: number;
+    volumeBuy: CurrencyValueMap;
+    volumeSell: CurrencyValueMap;
+    volumeChange: CurrencyValueMap;
+    marketCap: CurrencyValueMap;
+    marketCapChange: CurrencyValueMap;
+    marketCapChangePercents: CurrencyValueMap;
+    liquidity: CurrencyValueMap;
+    holdersCount: number;
+    holdersCountChange: number;
+    security: SecurityFeature[];
+    links: Link[];
+};
+
+export interface ResponseData {
+    items: Item[];
+    totalCount: number;
+};
